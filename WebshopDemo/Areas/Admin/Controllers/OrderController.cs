1e60518c6948e8fa7dab8431e312294d4b57c59c
hcosic22 <hrvoje.cosic@73strings.com>
2024-02-26 21:07:28 +0100

    Dodani controlleri za kategoriju, oreder i product.

---
+++ WebshopDemo/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.ComponentModel;
+using Webshop.Models;
+using WebshopDemo.Data;
+
+namespace WebshopDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var orders = await _context.Order.ToListAsync();
+
+            return View(orders);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var order = await _context.Order.FirstOrDefaultAsync(o => o.Id == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            //var orderProducts = await _context.OrderProduct.Where(p => p.OrderId == id).ToListAsync();
+
+            //order.OrderProducts = orderProducts;
+
+            return View(order);
+        }
+
+        public async Task<IActionResult> Create() 
+        {
+            ViewBag.Users = await _context.Users.Select(user =>
+                new SelectListItem 
+                { 
+                    Value = user.Id.ToString(),
+                    Text = user.FirstName + " " + user.LastName
+                }
+            ).ToListAsync();
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DateCreated,Total,UserId")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return  RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var order = await _context.Order.FindAsync(id);
+            if (order == null) 
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DateCreated,Total,UserId")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Order.Any(o => o.Id == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var order = await _context.Order.FirstOrDefaultAsync(o => o.Id == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var order = await _context.Order.FindAsync(id);
+            _context.Order.Remove(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- WebshopDemo/Areas/Admin/Controllers/ProductController.cs
+++ WebshopDemo/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Permissions;
 using Webshop.Models;
 using WebshopDemo.Data;
 
@@ -9,32 +9,32 @@ namespace WebshopDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Authorize(Roles = "Admin")]
-    public class CategoryController : Controller
+    public class ProductController : Controller
     {
         private readonly ApplicationDbContext _context;
 
-        public CategoryController(ApplicationDbContext context)
+        public ProductController(ApplicationDbContext context)
         {
                 _context = context;
         }
 
         public async Task<IActionResult> Index()
         {
-            var categories = await _context.Category.ToListAsync();
+            var products = await _context.Product.ToListAsync();
 
-            return View(categories);
+            return View(products);
         }
 
         public async Task<IActionResult> Details(int id)
         {
-            var category = await _context.Category.FirstOrDefaultAsync(c => c.Id == id);
+            var product = await _context.Product.FirstOrDefaultAsync(o => o.Id == id);
 
-            if (category == null)
+            if (product == null)
             {
                 return NotFound();
             }
 
-            return View(category);
+            return View(product);
         }
 
         public IActionResult Create()
@@ -44,86 +44,80 @@ namespace WebshopDemo.Areas.Admin.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Category category)
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Quantity,Price")] Product product)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(category);
+                _context.Add(product);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-
-            return View(category);
+            return View(product);
         }
 
         public async Task<IActionResult> Edit(int id)
         {
-            var category = await _context.Category.FindAsync(id);
-
-            if (category == null) 
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
             {
                 return NotFound();
             }
 
-            return View(category);
+            return View(product);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Category category) 
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Quantity,Price")] Product product)
         {
-            if (id != category.Id)
+            if (id != product.Id)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 try
                 {
-                    _context.Update(category);
+                    _context.Update(product);
                     await _context.SaveChangesAsync();
                 }
-                catch (DbUpdateConcurrencyException ex)
+                catch (DbUpdateConcurrencyException)
                 {
-                    if (!CategoryExists(category.Id))
+                    if (!_context.Product.Any(o => o.Id == id))
                     {
                         return NotFound();
                     }
                     else
                     {
-                        throw ex;
+                        throw;
                     }
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(category);
+            return View(product);
         }
 
         public async Task<IActionResult> Delete(int id)
         {
-            var category = await _context.Category.FirstOrDefaultAsync(c => c.Id == id);
-            if (category == null) 
+            var product = await _context.Product.FirstOrDefaultAsync(o => o.Id == id);
+
+            if (product == null)
             {
                 return NotFound();
             }
 
-            return View(category);
+            return View(product);
         }
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var category = await _context.Category.FindAsync(id);
-            _context.Category.Remove(category);
+            var product = await _context.Product.FindAsync(id);
+            _context.Product.Remove(product);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool CategoryExists(int id)
-        {
-            return _context.Category.Any(e => e.Id == id);
         }
     }
 }
